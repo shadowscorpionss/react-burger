6e64c75c6ed385576e525588888fcbe17504e614
shadowscorpionss <shadowscorpionss@outlook.com>
2023-09-30 12:12:19 +0500

    add loading data from api

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import AppHeader from "../app-header/app-header";
 import appStyles from "./app.module.css";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
@@ -6,7 +6,30 @@ import utdata from "../../utils/data";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 
 function App (){
-  const [state, setState] = useState({ingdata: utdata});
+  const [state, setState] = useState({
+    ingdata: utdata, //default data from file
+    isLoading: false,
+    hasError: false,
+  });
+  
+  //call api and fetch data  
+  function getData() {
+    setState({ ...state, hasError: false, isLoading: true });
+
+    fetch("https://norma.nomoreparties.space/api/ingredients")
+      .then(response => response.json())
+      .then(obj => setState({ ...state, ingdata: obj.data, hasError: !obj.success,  isLoading: false }))
+      .catch(err => {
+        setState({ ...state, hasError: true, isLoading: false });
+        console.log("I've caught it! ",err);
+      })
+  }
+
+  //loading data on mount
+  useEffect (()=>{
+    return getData()    
+  },[]);
+
 
   return (
     <div className="App">
