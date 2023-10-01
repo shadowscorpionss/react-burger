b2180493b92ebb601a6e1301969cc572c68b0934
shadowscorpionss <shadowscorpionss@outlook.com>
2023-10-01 15:22:46 +0500

    added useToggle hook

---
+++ src/components/hooks/useToggle.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export const useToggle = (initialValue) => {
+    const [isOpened, setIsOpened] = useState(() => {
+      if (typeof initialValue === "function") {
+        return initialValue();
+      }
+  
+      return initialValue;
+    });
+  
+    const toggle = () => {
+      setIsOpened((prev) => !prev);
+    };
+
+    const open = () => {
+        setIsOpened(true);
+      };
+
+    const close = () => {
+        setIsOpened(false);
+      };
+  
+  
+    return [ isOpened, toggle, open, close];
+  };
