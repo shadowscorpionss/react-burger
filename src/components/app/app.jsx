d2cc4c144009bcd6770cef34df10141434f587b8
shadowscorpionss <shadowscorpionss@outlook.com>
2023-10-28 10:37:42 +0500

    moved homepage to pages

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,33 +1,15 @@
-import { useState, useEffect, useContext, useReducer } from "react";
 import AppHeader from "../app-header/app-header";
-import appStyles from "./app.module.css";
-import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import BurgerConstructor from "../burger-constructor/burger-constructor";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import { Route, Routes } from "react-router-dom";
+import { HomePage } from "../../pages";
+import appStyles from './app.module.css';
 
 function App() {
-  const [orderId, setOrderId] = useState(0);
   return (
-    <div className="App">
+    <div className={appStyles.App}>
       <AppHeader />
-      <main>
-        <div className={appStyles.grid}>
-          <DndProvider backend={HTML5Backend}>
-            <section className={appStyles.column}>
-              <h2 className="text text_type_main-large">Соберите бургер</h2>
-              <div className={appStyles.cellContent}>
-                <BurgerIngredients />
-              </div>
-            </section>
-            <section className={appStyles.column}>
-              <div className={appStyles.cellContent}>
-                <BurgerConstructor />
-              </div>
-            </section>
-          </DndProvider>
-        </div>
-      </main>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
