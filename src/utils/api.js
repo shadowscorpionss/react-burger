b83d36401f0e2e236c1ce93490cda8d3c4805732
shadowscorpionss <shadowscorpionss@outlook.com>
2023-10-28 23:56:13 +0500

    api: added new auth request methods

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-export const NORMA_API = 'https://norma.nomoreparties.space/api';
+export const NORMA_API = "https://norma.nomoreparties.space/api";
 
 function checkResponse(res) {
     return res.ok ? res.json() : res.json().then(err => Promise.reject(err))
@@ -20,12 +20,12 @@ export function request(endpoint, options) {
 }
 
 export function getIngredientsRequest() {
-    return request('ingredients');
+    return request("ingredients");
 }
 
 export function postRequest(endpoint, data) {
     return request(endpoint, {
-        method: 'POST',
+        method: "POST",
         headers: {
             "Content-Type": "application/json;charset=utf-8",
         },
@@ -34,5 +34,49 @@ export function postRequest(endpoint, data) {
 }
 
 export function postOrderRequest(data) {
-    return postRequest('orders', { ingredients: data });
+    return postRequest("orders", { ingredients: data });
 }
+
+export function getUser(token) {
+    return fetch(`${NORMA_API}/auth/user`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token
+        },
+    }).then(checkResponse)
+}
+
+
+function postAuthRequest(authEndpoint, data){
+    return postRequest(`auth/${authEndpoint}`, data)
+    .then(checkResponse);
+}
+
+
+export function loginRequest(email, password) {
+    return postAuthRequest("login",{email,password});
+}
+
+export function refreshTokens(token) {
+    return postAuthRequest("token", {token});
+}
+
+
+export function registrationRequest(email, password, name) {
+    return postAuthRequest("register", {email,password,name});
+}
+
+
+export function passwordReset(email) {
+    return postRequest("password-reset",{email});
+}
+
+
+export function passwordRecovery(password, token) {
+    return postRequest("password-reset/reset",{password,token});
+}
+
+
+
+
