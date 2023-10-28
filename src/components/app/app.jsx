36b059b57e205735dfdf506e97174f7b0a5e9e0d
shadowscorpionss <shadowscorpionss@outlook.com>
2023-10-29 03:53:35 +0500

    added protected user pages on main (into routes)

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,22 @@
 import AppHeader from "../app-header/app-header";
 import { Route, Routes } from "react-router-dom";
-import { HomePage } from "../../pages";
+import { FORGOT_PATH, ForgotPasswordPage, HOME_PATH, HomePage, LOGIN_PATH, LoginPage, REGISTER_PATH, RESET_PATH, RegisterPage, ResetPasswordPage } from "../../pages";
 import appStyles from './app.module.css';
+import { ProtectedUserRoute } from "../protected-user-route/protected-user-route";
 
 function App() {
   return (
     <div className={appStyles.App}>
       <AppHeader />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path={HOME_PATH} element={<HomePage />} />
+
+        {/*  Protected user pages  */}
+        <Route path={LOGIN_PATH} element={<ProtectedUserRoute><LoginPage /></ProtectedUserRoute>} />
+        <Route path={REGISTER_PATH} element={<ProtectedUserRoute><RegisterPage /></ProtectedUserRoute>} />
+        <Route path={FORGOT_PATH} element={<ProtectedUserRoute><ForgotPasswordPage /></ProtectedUserRoute>} />
+        <Route path={RESET_PATH} element={<ProtectedUserRoute><ResetPasswordPage /></ProtectedUserRoute>} />
+        
       </Routes>
     </div>
   );
