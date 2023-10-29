9a343220324a33adf8dad5b07c79985e2033ec31
shadowscorpionss <shadowscorpionss@outlook.com>
2023-10-29 18:55:56 +0500

    added pages paths

--- src/pages/pages-paths.js
+++ src/pages/pages-paths.js
@@ -3,3 +3,6 @@ export const LOGIN_PATH="/login";
 export const REGISTER_PATH="/register";
 export const FORGOT_PATH="/forgot-password";
 export const RESET_PATH="/reset-password";
+export const PROFILE_PATH="/profile";
+export const PROFILE_ORDERS_PATH="/profile/orders";
+export const FEED_PATH="/feed";
