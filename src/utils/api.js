76a1da55aa9f64bf12b39cb2f17a0ac9e86277f8
shadowscorpionss <shadowscorpionss@outlook.com>
2023-10-29 18:59:37 +0500

    API: added update user method

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,16 @@
 export const NORMA_API = "https://norma.nomoreparties.space/api";
 
+const JSON_CONTENT_TYPE = "application/json;charset=utf-8";
+
 function checkResponse(res) {
     return res.ok ?
         res.json() :
         res.json().then(err => Promise.reject({
+            //передаем статус (401, 403, 500 и тд)
             status: res.status,
+            //адрес страницы с ошибкой и statusText если есть
             additional: `${res.url} ${res.statusText}`,
+            //сообщение из json
             message: err.message
         }));
 }
@@ -19,8 +24,42 @@ function checkSuccess(res) {
     return Promise.reject(`Что-то пошло не так: ${res}`);
 };
 
+
+function addAuthorization(accessToken, options) {
+    return {
+        ...options, headers: {
+            ...options.headers,
+            Authorization: "Bearer " + accessToken
+        }
+    };
+}
+
+function addJsonContentType(options) {
+    return {
+        ...options,
+        headers: {
+            ...options.headers,
+            "Content-Type": JSON_CONTENT_TYPE
+        }
+    }
+}
+
+function createOptions(method) {
+    return {
+        method: method,
+    }
+}
+
+function addData (data, options){
+    return {
+        ...options,
+        body: JSON.stringify(data)
+    }
+}
+
+
 export function request(endpoint, options) {
-    return fetch(`${NORMA_API}/${endpoint}`, options)
+        return fetch(`${NORMA_API}/${endpoint}`, options)
         .then(checkResponse)
         .then(checkSuccess);
 }
@@ -29,64 +68,61 @@ export function getIngredientsRequest() {
     return request("ingredients");
 }
 
-export function postRequest(endpoint, data) {
+export function postRequest(endpoint, data, options={}) {    
     return request(endpoint, {
+        ...options,
         method: "POST",
         headers: {
-            "Content-Type": "application/json;charset=utf-8",
+            ...options.headers,
+            "Content-Type": JSON_CONTENT_TYPE,
         },
         body: JSON.stringify(data)
     })
 }
 
-export function postOrderRequest(data) {
-    return postRequest("orders", { ingredients: data });
+export function postOrderRequest(data, accessToken) {
+    return postRequest("orders",
+        { ingredients: data },
+        addAuthorization(accessToken));
 }
 
-export function getUser(token) {
-    return fetch(`${NORMA_API}/auth/user`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + token
-        },
-    }).then(checkResponse)
+export function getUser(accessToken) {
+    return request(`/auth/user`,
+        addAuthorization(accessToken,
+            addJsonContentType(createOptions("GET"))));
 }
 
 
 function postAuthRequest(authEndpoint, data) {
-    return postRequest(`auth/${authEndpoint}`, data)
-        .then(checkResponse);
+    return postRequest(`auth/${authEndpoint}`, data);        
 }
 
 
 export function loginRequest(email, password) {
-    return postAuthRequest("login", { email, password })
-        .then(checkSuccess);
+    return postAuthRequest("login", { email, password });
 }
 
-export function refreshTokens(token) {
-    return postAuthRequest("token", { token });
+export function logoutRequest(refreshToken) {
+    return postAuthRequest("logout", { token: refreshToken });
 }
 
+export function refreshTokens(refreshToken) {
+    return postAuthRequest("token", { token: refreshToken });
+}
 
 export function registrationRequest(email, password, name) {
-    return postAuthRequest("register", { email, password, name })
-        .then(checkSuccess);
+    return postAuthRequest("register", { email, password, name });
 }
-
 
 export function passwordReset(email) {
-    return postRequest("password-reset", { email })
-        .then(checkSuccess);
+    return postRequest("password-reset", { email });
 }
-
 
 export function passwordRecovery(password, token) {
-    return postRequest("password-reset/reset", { password, token })
-        .then(checkSuccess);
+    return postRequest("password-reset/reset", { password, token });
 }
 
-
-
-
+export function updateUser(name, email, password, accessToken) {
+    const options = addData({name,email,password}, addAuthorization(accessToken ,createOptions("PATCH")));
+    return request("/auth/user",  options);
+}
