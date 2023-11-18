d66fdf21adda4633612a0ca3d66b749989788cd2
shadowscorpionss <shadowscorpionss@outlook.com>
2023-11-19 01:22:55 +0500

    added types to utils/data

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,4 +1,6 @@
-const data= [
+import { IIngredient } from "../types/ingredient-types";
+
+const data:Array<IIngredient> = [
     {
        "_id":"60666c42cc7b410027a1a9b1",
        "name":"Краторная булка N-200i",
