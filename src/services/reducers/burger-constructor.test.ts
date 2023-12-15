a9d3317f1eaa2af88fc9fee1172279954a965fc2
Грачев Максим <crmig@rus.ru>
2023-12-15 10:51:17 +0300

    added burger-constror reducer test

--- src/services/reducers/burger-constructor.test.ts
+++ src/services/reducers/burger-constructor.test.ts
@@ -1,74 +1,126 @@
-import { IOrderStorage, TOrder, TOrderStorage, defaultOrder } from '../../types/order-types';
-import { GET_CURRENT_ORDER_FAILED, GET_CURRENT_ORDER_REQUEST, GET_CURRENT_ORDER_SUCCESS, TGetCurrentOrderActions } from '../actions/current-order';
-import { MAKE_ORDER_FAILED, MAKE_ORDER_REQUEST, MAKE_ORDER_SUCCESS, TMakeOrderActions } from '../actions/order';
+
+import exp from 'constants';
+import { ITheIngredient, TBurgerConstructorStorage } from '../../types/constructor-types';
+import { IIngredient } from '../../types/ingredient-types';
+import { ADD_CONSTRUCTOR_INGREDIENT, CLEAR_CONSTRUCTOR_DATA, REMOVE_CONSTRUCTOR_INGREDIENT, SET_CONSTRUCTOR_BUN, SORT_CONSTRUCTOR_DATA, TBurgerConstructorActions, addUniqueId } from '../actions/burger-constructor';
+import { GET_CURRENT_ORDER_FAILED, GET_CURRENT_ORDER_SUCCESS } from '../actions/current-order';
+import { burgerConstructorReducer } from './burger-constructor';
 import { currentOrderReducer } from './current-order';
-import { orderReducer } from './order';
 
-const order: TOrder =
-{
-    _id: '657af14d7fd657001ba0916b',
-    ingredients: ['643d69a5c3f7b9001cfa093d',
-        '643d69a5c3f7b9001cfa093e',
-        '643d69a5c3f7b9001cfa0945',
-        '643d69a5c3f7b9001cfa093d'],
-    status: 'done',
-    name: 'Антарианский люминесцентный флюоресцентный бургер',
-    createdAt: '2023-12-14T12:13:01.548Z',
-    updatedAt: '2023-12-14T12:13:01.968Z',
-    number: 29175,
-
+const defaultBun: ITheIngredient = {
+    uniqueId: '22',
+    _id: '',
+    price: 0,
+    name: 'default',
+    image: '',
+    image_mobile: '',
+    image_large: '',
+    type: 'bun',
+    __v: 0
 };
 
-const initialState: IOrderStorage = {
-    order: defaultOrder,
-    isLoading: false,
-    isFailed: false,
-    errorMessage: ''
+const newBun: ITheIngredient = {
+    uniqueId: '33',
+    _id: '',
+    price: 1,
+    name: 'newBun',
+    image: '',
+    image_mobile: '',
+    image_large: '',
+    type: 'bun',
+    __v: 0
+
+}
+
+
+
+const newMain: ITheIngredient = {
+    _id: '',
+    price: 0,
+    name: 'testMain',
+    image: '',
+    image_mobile: '',
+    image_large: '',
+    type: 'main',
+    __v: 0,
+    uniqueId: '1'
+}
+
+const newSauce: ITheIngredient = {
+    _id: '',
+    price: 0,
+    name: 'testSauce',
+    image: '',
+    image_mobile: '',
+    image_large: '',
+    type: 'sauce',
+    __v: 0,
+    uniqueId: '1'
+}
+
+const unsorted = [newMain, newSauce];
+const sorted = [newSauce,newMain]
+
+
+const initialState: TBurgerConstructorStorage = {
+    bun: defaultBun,
+    ingredients: []
+}
+const addedState: TBurgerConstructorStorage = {
+    bun: defaultBun,
+    ingredients: [newMain]
 }
 
 describe('test currentOrderReducer', () => {
     it('should return the initial state', () => {
-        expect(currentOrderReducer(undefined, {} as TGetCurrentOrderActions)).toEqual(initialState)
+        const result = burgerConstructorReducer(undefined, {} as TBurgerConstructorActions)
+        expect(result.bun).not.toBeUndefined()
+        expect(result.bun.type).toEqual('bun')
+        expect(result.ingredients).not.toBeNull()
+        expect(result.ingredients.length).toEqual(0)
     });
 
-    it('should return loading state on request', () => {
-        const expected = {
-            ...initialState,
-            isLoading: true,
-            isFailed: false
-        };
-        const received = currentOrderReducer(initialState, {
-            type: GET_CURRENT_ORDER_REQUEST
+    it('should add constructor ingredient', () => {
+        const result = burgerConstructorReducer(initialState, {
+            type: ADD_CONSTRUCTOR_INGREDIENT,
+            ingredient: newMain
         });
-        expect(received).toEqual(expected)
+        expect(result.ingredients.length).toBe(1)
     });
 
-    it('should return order if success', () => {
-        const expected = {
-            ...initialState,
-            isLoading: false,
-            isFailed: false,
-            order: order
-        };
-        const received = currentOrderReducer(initialState, {
-            type: GET_CURRENT_ORDER_SUCCESS,
-            order: order
+    it('should remove constructor ingredient', () => {
+        let result = burgerConstructorReducer(addedState, {} as TBurgerConstructorActions);
+        expect(result.ingredients.length).toEqual(1)
+        result = burgerConstructorReducer(result, {
+            type: REMOVE_CONSTRUCTOR_INGREDIENT,
+            uniqueId: '1'
         });
-        expect(received).toEqual(expected)
+        expect(result.ingredients.length).toEqual(0)
     });
 
-    it('should return error message if failed', () => {
-        const expected = {
-            ...initialState,
-            isFailed: true,
-            isLoading: false,
-            errorMessage: 'failed'
-        };
-        const received = currentOrderReducer(initialState, {
-            type: GET_CURRENT_ORDER_FAILED,
-            err: { message: 'failed', status: 0, additional: '' }
-        });
-        expect(received).toEqual(expected)
+    it('should return the initial state after clear', () => {
+        const result = burgerConstructorReducer(addedState, { type: CLEAR_CONSTRUCTOR_DATA })
+        expect(result.bun).not.toBeUndefined()
+        expect(result.bun.type).toEqual('bun')
+        expect(result.ingredients).not.toBeNull()
+        expect(result.ingredients.length).toEqual(0)
     });
+
+    it('should set new bun', () => {
+        const result = burgerConstructorReducer(initialState, {type:SET_CONSTRUCTOR_BUN, bun: newBun});        
+        expect(result.bun.type).toEqual('bun')
+        expect(result.bun).not.toEqual(defaultBun)
+        expect(result.bun).toEqual(newBun)
+    });
+
+    it('should ingredients be sorted', ()=> {
+        let result = burgerConstructorReducer(initialState,{}as TBurgerConstructorActions);
+        unsorted.forEach(v=> result=burgerConstructorReducer(result, {type: ADD_CONSTRUCTOR_INGREDIENT, ingredient: v}));
+        expect(result.ingredients).toEqual(unsorted)
+        expect(result.ingredients).not.toEqual(sorted)
+        result=burgerConstructorReducer(result, {type: SORT_CONSTRUCTOR_DATA, ingredients: sorted});
+        expect(result.ingredients).toEqual(sorted)
+        expect(result.ingredients).not.toEqual(unsorted)        
+    })
 
 });
